22c8edfece9d02d08ef1cd0130287fe117e3421c
Niels <nga.smits@student.avans.nl>
2019-05-03 17:41:16 +0200

    Brams code gecommend

--- src/controller/InlogController.java
+++ src/controller/InlogController.java
@@ -1,23 +1,23 @@
-package controller;
-
-import View.InlogPane;
-import model.InlogModel;
-
-public class InlogController {
-
-	private InlogPane inlogPane;
-	private InlogModel inlogModel;
-
-	public InlogController(InlogModel inlogModel, InlogPane inlogPane) {
-		this.inlogPane = inlogPane;
-		this.inlogModel = inlogModel;
-
-		
-		// this calls the button from the inlog view and calls the methode from the inlogmodel.
-		inlogPane.getLoginButton().setOnAction(e -> inlogModel.handleLogin());
-
-		// this calls the button from the inlog view and calls the methode from the inlogmodel.
-		inlogPane.getRegisterButton().setOnAction(e -> inlogModel.handleRegister());
-	}
-
-}
+//package controller;
+//
+//import View.InlogPane;
+//import model.InlogModel;
+//
+//public class InlogController {
+//
+//	private InlogPane inlogPane;
+//	private InlogModel inlogModel;
+//
+//	public InlogController(InlogModel inlogModel, InlogPane inlogPane) {
+//		this.inlogPane = inlogPane;
+//		this.inlogModel = inlogModel;
+//
+//		
+//		// this calls the button from the inlog view and calls the methode from the inlogmodel.
+//		inlogPane.getLoginButton().setOnAction(e -> inlogModel.handleLogin());
+//
+//		// this calls the button from the inlog view and calls the methode from the inlogmodel.
+//		inlogPane.getRegisterButton().setOnAction(e -> inlogModel.handleRegister());
+//	}
+//
+//}
